b398f5f632694026df12eaf414833d9baf945678
FixterSpb <Fixter.spb@gmail.com>
2020-06-30 17:44:37 +0300

    Выполнено задание 5 урока

--- 5/scripts/game.js
+++ 5/scripts/game.js
@@ -1,8 +1,8 @@
 'use strict';
 renderer.renderBoard();
 //Назначаем обработчики нажатия мыши
-
+/*
 let cells = document.querySelectorAll('i');
 cells.forEach(cell => cell.addEventListener('click', function (event) {
     mover.onClick(event);
-}));
+}));*/
